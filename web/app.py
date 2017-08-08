5aa236e86fc7913a1ca18f0df94100c1a2222450
Kosate Limpongsa <kosatelim@gmail.com>
2017-08-09 00:37:15 +0700

    Complete integrate with sketch.js

--- web/app.py
+++ web/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True)
